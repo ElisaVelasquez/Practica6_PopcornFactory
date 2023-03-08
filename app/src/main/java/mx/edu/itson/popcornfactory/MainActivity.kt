f948035ca5ffcad8829c7e7f1618c0351807123d
ElisaVelasquez <101837234+ElisaVelasquez@users.noreply.github.com>
2023-03-08 12:15:25 -0700

    Practica 7
    Practica 7 completada

--- app/src/main/java/mx/edu/itson/popcornfactory/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/popcornfactory/MainActivity.kt
@@ -1,9 +1,9 @@
 package mx.edu.itson.popcornfactory
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.botonP)
 
         button.setOnClickListener {
-            var intent: Intent = Intent( this, Catalogo::class.java)
+            var intent: Intent = Intent(this, Catalogo::class.java)
             startActivity(intent)
         }
     }
